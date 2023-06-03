4b45e1b80d51b936194a8d7c0c8cfca6cacb4681
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2023-06-03 06:59:02 +0200

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+const cells = document.querySelectorAll('td');
+const piecesContainer = document.getElementById('pieces-container');
+
+piecesContainer.addEventListener('click', (event) => {
+  const piece = event.target.closest('.container');
+  if (!piece) return;
+
+  const selectedPiece = document.querySelector('.selected-piece');
+  if (selectedPiece) {
+    selectedPiece.classList.remove('selected-piece');
+  }
+  piece.classList.add('selected-piece');
+});
+
+cells.forEach(cell => {
+  cell.addEventListener('click', () => {
+    const selectedPiece = document.querySelector('.selected-piece');
+    if (selectedPiece && cell.innerHTML === '') {
+      cell.appendChild(selectedPiece);
+      selectedPiece.classList.remove('selected-piece');
+    }
+  });
+});
