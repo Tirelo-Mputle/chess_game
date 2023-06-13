9c8df3b282b584b39b0c646de2a7035291da0fec
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2023-06-13 10:13:15 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,28 @@
-// Neo's code:
-// const cells = document.querySelectorAll('td');
-// const piecesContainer = document.getElementById('pieces-container');
+const body = document.querySelector('body')
 
-// piecesContainer.addEventListener('click', (event) => {
-//   const piece = event.target.closest('.container');
-//   if (!piece) return;
+const square = document.querySelectorAll('.square')
 
-//   const selectedPiece = document.querySelector('.selected-piece');
-//   if (selectedPiece) {
-//     selectedPiece.classList.remove('selected-piece');
-//   }
-//   piece.classList.add('selected-piece');
-// });
+body.addEventListener('click', (e) => {
+    const piece = event.target.closest('[data-pieces]')
 
-// cells.forEach(cell => {
-//   cell.addEventListener('click', () => {
-//     const selectedPiece = document.querySelector('.selected-piece');
-//     if (selectedPiece && cell.innerHTML === '') {
-//       cell.appendChild(selectedPiece);
-//       selectedPiece.classList.remove('selected-piece');
-//     }
-//   });
-// });
+    if (!piece) return
+
+    const selectedPiece = document.querySelector('.selected-piece');
+
+    if (selectedPiece) {
+        selectedPiece.classList.remove('selected-piece')
+    }
+
+    piece.classList.add('selected-piece')
+})
+
+square.forEach(block => {
+    block.addEventListener('click', () => {
+        const selectedPiece = document.querySelector('.selected-piece');
+
+        if(selectedPiece && block.innerHTML === '') {
+            block.appendChild(selectedPiece);
+            selectedPiece.classList.remove('selectedPiece')
+        }
+    })
+})
